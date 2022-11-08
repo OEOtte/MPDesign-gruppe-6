de13496dfadc0d4a398c8c4acaa24a29ae93f731
Thomas-Frederiksen <thomas.frederiksen07@gmail.com>
2022-11-08 13:37:36 +0100

    findByPhone

--- MiniProjektDesign/src/Controller/PersonController.java
+++ MiniProjektDesign/src/Controller/PersonController.java
@@ -1,5 +1,19 @@
 package Controller;
 
+import model.Person;
+import model.PersonContainer;
+
 public class PersonController {
 
+	public Person createPerson(String name, int phone, String Address, String City, int postalCode) {
+		Person p = new Person(name, phone, address, city, postalCode);
+		PersonContainer.getInstance().addPerson(p);
+		return p;
+	}
+	
+	public Person findByPhone(int phone) {
+		PersonContainer pc = PersonContainer.getInstance();
+		Person p = pc.findByPhone(phone);
+		
+	}
 }

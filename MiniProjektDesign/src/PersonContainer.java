5f9c6ec67d01f071687eef96c57966e8b884004c
Thomas-Frederiksen <thomas.frederiksen07@gmail.com>
2022-11-08 13:35:09 +0100

    findByPhone made

---
+++ MiniProjektDesign/src/PersonContainer.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+
+public class PersonContainer {
+	private static PersonContainer instance;
+	private ArrayList<Person> people;
+
+	public static PersonContainer getInstance() {
+		if(instance == null) {
+			instance = new PersonContainer();
+		}
+		return instance;
+	}
+	private PersonContainer() {
+		people = new ArrayList<>();
+	}
+	public void addPerson(Person person) {
+		people.add(person);//TODO check correct input - null and duplicates
+	}
+
+	public ArrayList<Person> getCurrentPeople() {
+		return people;
+	}
+	
+	public Person findByPhone(int phone) {
+		Person res = null;
+		for(int i = 0 ; i < people.size() && res == null; i++) {
+			if(people.get(i).getPhone().equals(phone)) {
+				res = people.get(i);
+			}
+			return people.get(i);
+		}
+		
+	}
+}

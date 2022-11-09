10b416dbbe4e91a6ad48fc0163f5e0bbebe2fbd2
ScriptKittye <filipulsoe@gmail.com>
2022-11-09 09:36:42 +0100

    Update LoanCtroller.java

--- MiniProjektDesign/src/Controller/LoanCtroller.java
+++ MiniProjektDesign/src/Controller/LoanCtroller.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import model.Copy;
 import model.Loan;
@@ -8,34 +9,41 @@ import model.LoanContainer;
 import model.Person;
 
 public class LoanCtroller {
-	
+
 	private Person p;
 	private Loan aloan;
 
-	public LoanCtroller() {};
-	
+	public LoanCtroller() {
+	};
+
+	// Metode til at finde en "friend" ved hjælp af phone.
 	public void findPersonByPhone(int phone) {
 		PersonController pc = new PersonController();
 		p = pc.findByPhone(phone);
 
 	}
 
-	public Loan createLoan(int loanNumber, String borrowDate, String period, boolean state, String returnDate, Person person) {
+	//Method for creating a loan with use of Person and Copy.
+	public Loan createLoan(int loanNumber, String borrowDate, String period, boolean state, 
+			String returnDate, Person person) {
 		aloan = new Loan(loanNumber, borrowDate, period, state, returnDate);
 		aloan.setPerson(person);
-		
+
 		// TODO mangler logik, for at henvende til getCopy metoden
-		// getCopy(serialNumber);
-		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine().toLowerCase();
+		int serialNumber = Integer.parseInt(input);
+		getCopy(serialNumber);
+
 		LoanContainer.getInstance().addLoan(aloan);
 		return aloan;
 	}
-	
+	//Method used to stick our copy to "aloan".
 	private void getCopy(int serialNumber) {
 		LPController lpc = new LPController();
 		Copy copy = lpc.findCopy(serialNumber);
 		aloan.setCopy(copy);
-		//LoanContainer.getInstance().addLoan(aloan);
+		// LoanContainer.getInstance().addLoan(aloan);
 	}
 
 }
